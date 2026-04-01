08578dcf27850fd5643438099dd11c62f3d938a5
Yousef Said <jooasaid2004@gmail.com>
2026-04-01 17:05:37 +0200

    Exercise 1

--- Collection_Task/Program.cs
+++ Collection_Task/Program.cs
@@ -1,10 +1,73 @@
-﻿namespace Collection_Task
+﻿using System.Diagnostics;
+
+namespace Collection_Task
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-           
+            #region List
+
+            //List<int> grads = [85, 92, 78, 95, 88, 70, 100, 65];
+
+            // print all grades
+
+            //foreach (var grad in grads)
+            //{
+            //    Console.WriteLine(grad);
+            //}
+
+            // count of list
+
+            //Console.WriteLine($" Count : {grads.Count}");
+
+            // first and last grade
+
+            //Console.WriteLine(grads[0]);
+            //Console.WriteLine(grads[^1]);
+
+
+
+            // sort the list
+
+            //grads.Sort();
+            //foreach (var grad in grads)
+            //{
+            //    Console.WriteLine(grad);
+            //}
+
+            //Get the first grade above 90
+
+            //Console.WriteLine( grads.Find(x => x > 90 ));
+
+            // Get all grades below 75 (failing grades)
+
+            //List<int> SuccessGrades = grads.FindAll(x => x < 75);
+            //foreach (var grade in SuccessGrades)
+            //{
+            //    Console.WriteLine(grade);
+            //}
+
+
+            //Remove all failing grades (below 75)
+
+            //grads.RemoveAll(x => x < 75);
+            //foreach (var grad in grads)
+            //{
+            //    Console.WriteLine(grad);
+            //}
+
+
+            //Check if any grade equals 100
+
+            //Console.WriteLine(grads.Contains(100));
+
+            //List<string> marks = grads.ConvertAll(x =>$"Grade:{x.ToString()} " );
+            //foreach (var mark in marks)
+            //{
+            //    Console.WriteLine(mark);
+            //}
+            #endregion
         }
     }
 }
